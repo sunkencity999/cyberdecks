3bb4da919d3d076c2a88f81efde18630fe00cb25
Christopher Bradford <theroyalart@gmail.com>
2018-10-13 13:18:19 -0700

    added tests for edit method on posts and wrote code to pass

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,5 +27,6 @@ class PostsController < ApplicationController
 
 
   def edit
+	  @post = Post.find(params[:id])
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -65,5 +65,26 @@ RSpec.describe PostsController, :type => :controller do
 		  expect(response).to redirect_to Post.last
 	  end
   end
+
+  describe "GET edit" do
+	  it  "returns http success" do 
+		  get :edit, params: { id: my_post.id}
+		  expect(response).to have_http_status(:success)
+	  end
+	  it "renders the #edit view" do 
+		  get :edit, params: { id: my_post.id }
+		  expect(response).to render_template :edit
+	  end
+
+	  it "assigns post to be updated to @post" do
+		  get :edit, params: { id: my_post.id } 
+
+		  post_instance = assigns(:post)
+
+		  expect(post_instance.id).to eq my_post.id
+		  expect(post_instance.title).to eq my_post.title
+		  expect(post_instance.body).to eq my_post.body
+	  end
+  end
 end
   
