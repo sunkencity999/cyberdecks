669feef8329f22a5a18d735f71d1cf4f3ecb82d3
Christopher Bradford <theroyalart@gmail.com>
2018-10-14 23:50:51 -0700

    added topic model, wrote tests, wrote code to pass tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_205748) do
+ActiveRecord::Schema.define(version: 2018_10_15_063441) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2018_10_04_205748) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public", default: true
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
