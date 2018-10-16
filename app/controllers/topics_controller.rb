71f302ec6d53cef0a63ab91cfb86a55237c1b463
Christopher Bradford <theroyalart@gmail.com>
2018-10-16 12:18:30 -0700

    wrote tests to implement new method in topic controller, and then code to pass. Created view for making new Topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,4 +7,9 @@ class TopicsController < ApplicationController
 	def show
 		@topic = Topic.find(params[:id])
 	end
+
+	def new
+		@topic = Topic.new
+	end
+
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -35,4 +35,21 @@ RSpec.describe TopicsController, type: :controller do
 		end
 	end
 
+	describe "GET new" do 
+		it "returns http success" do
+			get :new 
+			expect(response).to have_http_status(:success)
+		end
+
+		it "renders the #new view" do 
+			get :new
+			expect(response).to render_template :new 
+		end
+
+		it "initializes @topic" do
+			get :new
+			expect(assigns(:topic)).not_to be_nil
+		end
+	end
+
 end
