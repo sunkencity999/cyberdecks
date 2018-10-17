45b59415ea59c5c4c355465ea0be4825f1b1b941
Christopher Bradford <theroyalart@gmail.com>
2018-10-17 00:13:49 -0700

    made partials and edited views, added validation for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,12 +5,19 @@ RSpec.describe Post, type: :model do
 	let(:name) { "TestName" }
 	let(:description) { "TestDescription" }
 	let(:title) { "TestTitle" }
-	let(:body) { "TestBody"}
+	let(:body) { "Test Body that is long enough to pass validation"}
 
 	let(:topic) { Topic.create!(name: name, description: description) }
 	let(:post) { topic.posts.create!( title: title, body: body) }
-
+	
+	#the methods for the below specs do not work correctly
 	#it { is_expected.to belong_to(:topic) }
+	#it { is_expected.to validate_presence_of(:title) }
+	#it { is_expected.to validate_presence_of(:body) }
+	#it { is_expected.to validate_presence_of(:topic) }
+	
+	#it { is_expected.to validate_length_of(:title).is_at_least(5) }
+	#it { is_expected.to validate_length_of(:body).is_at_least(15) }
 
 
 	describe "attributes" do
