b2dc5fc1872da784c1fa81ba0b37c582811abea1
Christopher Bradford <theroyalart@gmail.com>
2018-11-15 15:30:58 -0800

    added gravatars for users and implemented factory girl for user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,9 @@ class User < ApplicationRecord
 
 	enum role: [:member, :admin]
 
+	def avatar_url(size)
+		gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-	let(:user) { User.create!(name: "Test User", email: "test@test.com", password: "password")}
+	#let(:user) { User.create!(name: "Test User", email: "test@test.com", password: "password")}
+	let(:user) { create(:user) } 
 	it { is_expected.to have_many(:posts) }
 	it { is_expected.to have_many(:comments) }
 
 	describe "attributes" do
 		it " should have name and email attributes" do
-			expect(user).to have_attributes(name: "Test User", email: "test@test.com", password: "password")
+			#expect(user).to have_attributes(name: "Test User", email: "test@test.com", password: "password")
+			expect(user).to have_attributes(name: user.name, email: user.email)
 		end
 	
 		it "responds to role" do
@@ -52,6 +54,14 @@ RSpec.describe User, type: :model do
 			it "returns true for #admin?" do
 				expect(user.admin?).to be_truthy
 			end
+		end
+	end
+	describe ".avatar_url" do
+		let(:known_user) { create(:user, email: "blochead@bloc.io") }
+
+		it "retures the Gravatar URL for a known email entity" do
+			expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+			expect(known_user.avatar_url(48)).to eq(expected_gravatar)
 		end
 	end
 end
